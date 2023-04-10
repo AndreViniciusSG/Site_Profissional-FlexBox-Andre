3ede1da3cc1df24fde94d5dc99bb46e1e5f23790
AndreViniciusSG <andrevinicius_santos@hotmail.com>
2023-04-10 09:54:38 -0300

    Teste Formulario

--- javascript/alert.js
+++ javascript/alert.js
@@ -1,8 +1,14 @@
+var tempoAlerta = 3000; // 3 segundos
+
 function alerta(){
 	swal({
   title: "Mensagem enviada!",
   text: "Em breve retornarei o seu contato, desde já agradeço!",
   icon: "success",
 
-}5000);
+});
+function redirecionarProximaPagina() {
+  window.location.href = "https://andreviniciussg.github.io/Site_Profissional-FlexBox-Andre/contato.html";
+}
+setTimeout(redirecionarProximaPagina, tempoAlerta);
 }
