a5a0e481a889978fdb37da7b640f2f519d6149e6
AndreViniciusSG <andrevinicius_santos@hotmail.com>
2023-04-13 17:44:20 -0300

    Versão Nova
    Versão com alterações da tag <a> das redes sociais. Foi retirado momentaneamente a imagem home.

--- javascript/alert.js
+++ javascript/alert.js
@@ -1,14 +1,7 @@
-var tempoAlerta = 3000; // 3 segundos
-
 function alerta(){
 	swal({
-  title: "Mensagem enviada!",
-  text: "Em breve retornarei o seu contato, desde já agradeço!",
-  icon: "success",
-
+		title: "Mensagem enviada!",
+		text: "Em breve retornarei o seu contato, desde já agradeço!",
+		icon: "success",
 });
-function redirecionarProximaPagina() {
-  window.location.href = "https://andreviniciussg.github.io/Site_Profissional-FlexBox-Andre/contato.html";
-}
-setTimeout(redirecionarProximaPagina, tempoAlerta);
 }
